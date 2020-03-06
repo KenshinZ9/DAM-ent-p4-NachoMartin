50509efe2e656fe28f5819a26abe9ec1f0686551
DAM_1 <DAM_1@PB07>
2020-03-06 19:14:20 +0100

    usuario

--- src/mein/java/liceosorolla/Usuario.java
+++ src/mein/java/liceosorolla/Usuario.java
@@ -1,5 +1,52 @@
 package mein.java.liceosorolla;
 
 public class Usuario {
+	private String nombre;
+	private String apellidos;
+	private String fecha;
+	private int id;
+	
+	
+	
+	public Usuario(String nombre, String apellidos, String fecha, int id) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fecha = fecha;
+		this.id = id;
+	}
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setIdr(int id) {
+		this.id = id;
+	}
+	
 }
--- src/test/java/liceosorolla/UsuarioTest.java
+++ src/test/java/liceosorolla/UsuarioTest.java
@@ -1,5 +1,23 @@
 package test.java.liceosorolla;
 
-public class UsuarioTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import mein.java.liceosorolla.Usuario;
+
+
+public class UsuarioTest {
+	
+	private Usuario usuario;
+	@Before
+	public void Before() {
+		usuario = new Usuario("Nacho","Martin", "2020-03-6", 9);
+	}
+	@Test
+	public void comprobarNombre() {
+		assertEquals("Nacho", usuario.getNombre());
+	}
+	
 }
